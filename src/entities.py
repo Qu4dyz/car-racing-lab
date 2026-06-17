867830943cd6cb8f9aef48387f0788bcdf72632e
Quady <sergeiicherednik@gmail.com>
2026-06-17 14:36:11 +0300

    feat: added and auto-rotated car sprites

--- src/entities.py
+++ src/entities.py
@@ -1,19 +1,30 @@
 import pygame
 import random
+import os
 from src.settings import *
 
-
+# Вказуємо шлях до папки з картинками
+game_folder = os.path.dirname(os.path.dirname(__file__))
+img_folder = os.path.join(game_folder, 'img')
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.Surface((PLAYER_WIDTH, PLAYER_HEIGHT))
-        self.image.fill(PLAYER_COLOR)
-        self.rect = self.image.get_rect()
+        
+        try:
+            # 1. Завантажуємо картинку
+            img = pygame.image.load(os.path.join(img_folder, 'player_car.png')).convert_alpha()
+            # 2. Повертаємо на 90 градусів проти годинникової стрілки (капотом вгору)
+            img = pygame.transform.rotate(img, -90)
+            # 3. Масштабуємо
+            self.image = pygame.transform.scale(img, (PLAYER_WIDTH, PLAYER_HEIGHT))
+        except FileNotFoundError:
+            self.image = pygame.Surface((PLAYER_WIDTH, PLAYER_HEIGHT))
+            self.image.fill(PLAYER_COLOR)
 
+        self.rect = self.image.get_rect()
         self.rect.centerx = PLAYER_START_X
         self.rect.bottom = PLAYER_START_Y
-
         self.speed_x = 0
 
     def update(self):
@@ -35,10 +46,17 @@ class Player(pygame.sprite.Sprite):
 class Enemy(pygame.sprite.Sprite):
     def __init__(self, speed_y):
         super().__init__()
-        self.image = pygame.Surface((50, 80))
-        self.image.fill(BLUE)
-        self.rect = self.image.get_rect()
+        
+        try:
+            img = pygame.image.load(os.path.join(img_folder, 'enemy_car.png')).convert_alpha()
+            # Повертаємо на 90 градусів (щоб вороги їхали капотом вниз до тебе)
+            img = pygame.transform.rotate(img, 90)
+            self.image = pygame.transform.scale(img, (50, 80))
+        except FileNotFoundError:
+            self.image = pygame.Surface((50, 80))
+            self.image.fill(BLUE)
 
+        self.rect = self.image.get_rect()
         self.rect.x = random.randrange(0, SCREEN_WIDTH - self.rect.width)
         self.rect.y = random.randrange(-100, -40)
         self.speed_y = speed_y
